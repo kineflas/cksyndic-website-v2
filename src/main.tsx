98d038fd16fb1af9fe85f5ac5976c5b706bc7d6b
kineflas <khalidborntocode@gmail.com>
2026-05-02 12:24:23 +0100

    fix: restore hydrateRoot for react-snap SSG, reduces TBT

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
 import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import { createRoot, hydrateRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root')!;
+const app = (
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 );
+
+if (rootElement.hasChildNodes()) {
+  hydrateRoot(rootElement, app);
+} else {
+  createRoot(rootElement).render(app);
+}
